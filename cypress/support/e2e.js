10d3d2a33e2c77015445e0088f712a19e9d850e9
UsamaKhalid123 <136828763+UsamaKhalid123@users.noreply.github.com>
2023-11-17 18:22:54 +0500

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -5,3 +5,9 @@ require('./commands')
 // load and register the grep feature
 // https://github.com/bahmutov/cy-grep
 require('@bahmutov/cy-grep')()
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+  })
